0fbfbbe518315291fa9d9a968c9daa8473f214ea
Eric Larson <ewlarson@umn.edu>
2013-12-30 16:15:56 -0600

    Add: grunt task for running unit tests

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+/* jshint node: true */
+
+module.exports = function (grunt) {
+  "use strict";
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine')
+
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json')
+
+    , jasmine: {
+        src: "src/*.js"
+      , options: {
+          specs: "spec/*.js"
+        , vendor: "spec/javascripts/vendor/*.js"
+      }
+    }
+  })
+
+  grunt.registerTask('test', ['jasmine'])
+  grunt.registerTask('default', ['test'])
+};
